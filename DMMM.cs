9286489a963e482dfff4742fe050a329c5292c4a
hoangliem98 <1651012093liem@ou.edu.vn>
2018-10-22 16:32:22 +0700

    Init Project

---
+++ DMMM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class DMMM : UserControl
+    {
+        public DMMM()
+        {
+            InitializeComponent();
+        }
+
+        public string Us_Name
+        {
+            get { return label1.Text; }
+            set { label1.Text = value; }
+        }
+        public string Us_Name1
+        {
+            get { return label2.Text; }
+            set { label2.Text = value; }
+        }
+
+        public Image Us_Image
+        {
+            get { return pictureBox1.Image; }
+            set { pictureBox1.Image = value; }
+        }
+
+        public string Us_ButtonAdd
+        {
+            get { return button1.Text; }
+            set { button1.Text = value; }
+        }
+        public string Us_ButtonEdit
+        {
+            get { return button2.Text; }
+            set { button2.Text = value; }
+        }
+        public string Us_ButtonDelete
+        {
+            get { return button3.Text; }
+            set { button3.Text = value; }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btQLTT_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btQLS_Click(object sender, EventArgs e)
+        {
+            QLS frmS = new QLS();
+            this.Hide();
+            frmS.ShowDialog();
+            this.Show();
+        }
+
+
+        /*private void btnQLDM_Click(object sender, EventArgs e)
+        {
+            //Frm_QuanLyDanhMuc fr = new Frm_QuanLyDanhMuc();
+            this.Hide();
+            fr.ShowDialog();
+            this.Show();
+        }*/
+    }
+}
